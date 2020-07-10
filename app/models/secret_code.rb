fa30f48ddcaaed09decdd96331ee262b889e1c73
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-10 16:15:42 +0530

    Setup secret code model

---
+++ app/models/secret_code.rb
@@ -0,0 +1,2 @@
+class SecretCode < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+codes = ["everything", "is", "a", "code"]
+codes.each { |code| SecretCode.create(code: code) }
