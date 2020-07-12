1d422aeee6b21690feef3aa391082dd2f324b19d
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-12 19:10:36 +0530

    Signed in users details gets displays on the index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+
+  after_create :add_secret_code
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def add_secret_code
+    SecretCode.create(code: self.secret_code, email: self.email)
+  end
 end
