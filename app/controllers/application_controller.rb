009b1d33001ad45efb0f9b1f8e3d9758eab72d85
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-12 19:44:36 +0530

    Redirect to login page after logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,11 @@ class ApplicationController < ActionController::Base
       user.permit(:first_name, :last_name, :email, :password, :current_password)
     end
   end
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
