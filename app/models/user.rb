ca3d03ae4f7ecea2e2bdf4941fff9e26dcb38245
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-14 16:18:06 +0530

    Dont add a secret code if it already exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,11 @@ class User < ApplicationRecord
   private
 
   def add_secret_code
-    SecretCode.create(code: self.secret_code, email: self.email)
+    secret_code = SecretCode.find_by(code: self.secret_code)
+    if secret_code.blank?
+      SecretCode.create(code: self.secret_code, email: self.email)
+    else
+      secret_code.update_attributes(email: self.email)
+    end
   end
 end
