4e5fcacbc4177fa8e32f63118772d6dda7c42a8c
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-14 13:21:21 +0530

    Remove password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
+          # , :recoverable
 
   validates :email, uniqueness: true
   validates :secret_code, uniqueness: true
